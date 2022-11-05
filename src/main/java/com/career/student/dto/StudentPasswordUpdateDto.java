3833117d9cccedc53e7a1671e8d67c2fac489426
Sandhya <ranvijay7455@gmail.com>
2022-11-05 20:30:42 +0530

    StudentPasswordUpdate dto changed

--- src/main/java/com/career/student/dto/StudentPasswordUpdateDto.java
+++ src/main/java/com/career/student/dto/StudentPasswordUpdateDto.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class StudentPasswordUpdateDto {
 
-	@NotNull
+	@NotNull(message = "Email cannot be null")
 	private String email;
 	
 	@NotNull
