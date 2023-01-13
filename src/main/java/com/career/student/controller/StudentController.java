99d3df84bab5babb0f122da48c6113933736c105
SantoshTurerao <santosh.turerao@infinx.com>
2023-01-13 12:08:13 +0530

    added cross origin gloablaly

--- src/main/java/com/career/student/controller/StudentController.java
+++ src/main/java/com/career/student/controller/StudentController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -33,6 +34,7 @@ import com.career.student.service.StudentService;
 
 @RestController
 @RequestMapping("student")
+@CrossOrigin("*")
 public class StudentController {
 
 	@Autowired
