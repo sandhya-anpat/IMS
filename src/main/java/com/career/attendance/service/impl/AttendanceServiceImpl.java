7179290707d1f02ff5e2fd491c2b946ee03291b2
Vaibhav <dev.vaibhav99@gmail.com>
2023-03-18 15:51:02 +0530

    changed the input type of attendance upload controller to InputStream

--- src/main/java/com/career/attendance/service/impl/AttendanceServiceImpl.java
+++ src/main/java/com/career/attendance/service/impl/AttendanceServiceImpl.java
@@ -11,13 +11,11 @@ import org.apache.tomcat.util.http.fileupload.FileUploadException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.career.attendance.entity.Attendance;
 import com.career.attendance.repo.AttendanceRepo;
 import com.career.attendance.service.AttendanceService;
 import com.career.constants.AppConstants;
-import com.career.mentor.repo.MentorRepo;
 
 @Service
 public class AttendanceServiceImpl implements AttendanceService {
@@ -29,24 +27,37 @@ public class AttendanceServiceImpl implements AttendanceService {
 	private ModelMapper mapper;
 
 	@Override
-	public String uploadfile(MultipartFile file) throws FileUploadException {
+	public String uploadfile(InputStream inputStream, String filename) throws FileUploadException {
 		String response = AppConstants.ATTENDANCE_UPLOAD_FAILURE;
-		InputStream inputStream;
 		try {
-			inputStream = file.getInputStream();
 			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
 			String line;
 			Attendance attendance = null;
+			String sessionName = "";
+			String sessionDate = "";
+			if (filename != null) {
+				sessionName = filename.substring(0, filename.indexOf("_"));
+				sessionDate = filename.substring(filename.indexOf("_") + 1, filename.indexOf("."));
+				String dd = sessionDate.substring(0, 2);
+				String mm = sessionDate.substring(2,4);
+				String yyyy = sessionDate.substring(4);
+				sessionDate = yyyy+"-"+mm+"-"+dd;
+			}
 			List<Attendance> attendances = new ArrayList<Attendance>();
+			int count = 0;
 			while ((line = reader.readLine()) != null) {
+				if (++count == 1)
+					continue;
 				attendance = new Attendance();
 				String[] data = line.split("\\,", -1);
 				attendance.setName(data[0].trim());
 				attendance.setJoiningTime(data[2].trim());
 				attendance.setLeavingTime(data[3].trim());
 				attendance.setDuration(data[4].trim());
-				attendance.setFileName(file.getOriginalFilename());
+				attendance.setFileName(filename);
+				attendance.setSessionName(sessionName);
+				attendance.setDate(sessionDate);
 				attendances.add(attendance);
 			}
 			attendanceRepo.saveAllAndFlush(attendances);
